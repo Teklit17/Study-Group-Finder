187ad5c73b2a6ba8c07118ecf48879d0f6b5ad3a
teklit <teklit@uia.no>
2024-10-15 18:27:13 +0200

    add the Notification page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,12 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    
+    
+    public IActionResult Notification()
+    {
+        return View();
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
