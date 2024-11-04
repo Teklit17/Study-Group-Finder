ab554d73f5d769bff9b252fe4a3eb801f61452e0
your full name <sanderrg@uia.no>
2024-11-04 12:21:49 +0100

    Added Userprofile

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SG_Finder.Models;
-
-public class ApplicationUser : IdentityUser
+namespace SG_Finder.Models
 {
-    public required string Name { get; set; }
-    public int Age { get; set; }
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Navigation property to UserProfile
+        public virtual UserProfile UserProfile { get; set; }
+    }
 }
